8196e357007c095ec351a7ab2a8d0585d275236f
Thomas Pinder <tompinder@live.co.uk>
2021-01-09 21:53:57 +0000

    Package detection in setup.py

--- gpjax/__init__.py
+++ gpjax/__init__.py
@@ -1,9 +1,9 @@
 from jax.config import config
 config.update("jax_enable_x64", True)
+from . import gps
 from .kernel import RBF
 from .likelihoods import Gaussian
 from .mean_functions import ZeroMean
 from .utilities import save, load
-from . import gps
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
