5dad341eb693d59bb9bfe3106052671142ea1057
Tom Pinder <tompinder@live.co.uk>
2021-03-05 07:51:14 +0000

    Fix init sorting test

--- tests/parameters/test_base.py
+++ tests/parameters/test_base.py
@@ -12,23 +12,23 @@ def test_complete():
     posterior = Prior(kernel=RBF()) * Gaussian()
     partial_params = {"lengthscale": jnp.array(1.0)}
     full_params = complete(partial_params, posterior)
-    assert list(full_params.keys()) == ["lengthscale", "variance", "obs_noise"]
+    assert list(full_params.keys()) == sorted(["lengthscale", "variance", "obs_noise"])
 
 
 def test_initialise():
     posterior = Prior(kernel=RBF()) * Gaussian()
     params = initialise(posterior)
-    assert list(params.keys()) == ["lengthscale", "variance", "obs_noise"]
+    assert list(params.keys()) == sorted(["lengthscale", "variance", "obs_noise"])
 
 
 @pytest.mark.parametrize("n", [1, 10])
 def test_non_conjugate_initialise(n):
     posterior = Prior(kernel=RBF()) * Bernoulli()
     params = initialise(posterior, n)
-    assert list(params.keys()) == ["lengthscale", "variance", "latent"]
+    assert list(params.keys()) == sorted(["lengthscale", "variance", "latent"])
     assert params["latent"].shape == (n, 1)
 
 
 def test_hyperparametr_initialise():
     params = _initialise_hyperparams(RBF(), Zero())
-    assert list(params.keys()) == ["lengthscale", "variance"]
+    assert list(params.keys()) == sorted(["lengthscale", "variance"])
