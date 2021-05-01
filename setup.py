cc575ae0830ab5b912a22c836ec76f1a723db8f8
Thomas Pinder <t.pinder2@lancaster.ac.uk>
2021-05-01 19:45:56 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -29,13 +29,13 @@ EXTRAS = {
 
 setup(
     name="GPJax",
-    version="0.3.5",
+    version="0.3.6",
     author="Thomas Pinder",
     author_email="t.pinder2@lancaster.ac.uk",
     packages=find_packages(".", exclude=["tests"]),
     license="LICENSE",
-    description="Didactic Gaussian processes in Jax and ObJax.",
-    long_description="GPJax aims to provide a low-level interface to Gaussian process models. Code is written entirely in Jax and Objax to enhance readability, and structured so as to allow researchers to easily extend the code to suit their own needs. When defining GP prior in GPJax, the user need only specify a mean and kernel function. A GP posterior can then be realised by computing the product of our prior with a likelihood function. The idea behind this is that the code should be as close as possible to the maths that we would write on paper when working with GP models.",
+    description="Didactic Gaussian processes in Jax.",
+    long_description="GPJax aims to provide a low-level interface to Gaussian process models. Code is written entirely in Jax to enhance readability, and structured so as to allow researchers to easily extend the code to suit their own needs. When defining GP prior in GPJax, the user need only specify a mean and kernel function. A GP posterior can then be realised by computing the product of our prior with a likelihood function. The idea behind this is that the code should be as close as possible to the maths that we would write on paper when working with GP models.",
     install_requires=parse_requirements_file("requirements.txt"),
     extras_require=EXTRAS,
     keywords=["gaussian-processes jax machine-learning bayesian"],
