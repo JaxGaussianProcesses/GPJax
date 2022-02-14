0064ae76e94b1c60dcb6b0a15fd65f8e7e3a3ad6
Thomas Pinder <pindert2@lancaster.ac.uk>
2022-02-14 10:29:55 +0000

    Fix kernel

--- docs/nbs/graph_kernels.py
+++ docs/nbs/graph_kernels.py
@@ -8,7 +8,7 @@
 #       format_version: '1.3'
 #       jupytext_version: 1.11.5
 #   kernelspec:
-#     display_name: Python 3.9.7 ('gpjax')
+#     display_name: Python 3
 #     language: python
 #     name: python3
 # ---
--- docs/nbs/regression.py
+++ docs/nbs/regression.py
@@ -7,7 +7,7 @@
 #       format_version: '1.3'
 #       jupytext_version: 1.11.5
 #   kernelspec:
-#     display_name: GPJax
+#     display_name: Python 3
 #     language: python
 #     name: python3
 # ---
