452a9806f614d12a1e494357c736ce4498982f0f
Daniel-Dodd <daniel_dodd@icloud.com>
2022-05-20 16:03:17 +0100

    Update test_abstractions.py

--- tests/test_abstractions.py
+++ tests/test_abstractions.py
@@ -7,7 +7,7 @@ from jax.experimental import optimizers
 
 import gpjax as gpx
 from gpjax import RBF, Dataset, Gaussian, Prior, initialise, transform
-from gpjax.abstractions import fit, fit_batches, optax_fit
+from gpjax.abstractions import fit, fit_batches
 
 tfd = tf.data
 
@@ -22,27 +22,8 @@ def test_fit(n):
     params, trainable_status, constrainer, unconstrainer = initialise(p)
     mll = p.marginal_log_likelihood(D, constrainer, negative=True)
     pre_mll_val = mll(params)
-    opt_init, opt_update, get_params = optimizers.adam(step_size=0.1)
-    optimised_params = fit(
-        mll, params, trainable_status, opt_init, opt_update, get_params, n_iters=10
-    )
-    optimised_params = transform(optimised_params, constrainer)
-    assert isinstance(optimised_params, dict)
-    assert mll(optimised_params) < pre_mll_val
-
-
-@pytest.mark.parametrize("n", [20])
-def test_optax_fit(n):
-    key = jr.PRNGKey(123)
-    x = jnp.sort(jr.uniform(key=key, minval=-2.0, maxval=2.0, shape=(n, 1)), axis=0)
-    y = jnp.sin(x) + jr.normal(key=key, shape=x.shape) * 0.1
-    D = Dataset(X=x, y=y)
-    p = Prior(kernel=RBF()) * Gaussian(num_datapoints=n)
-    params, trainable_status, constrainer, unconstrainer = initialise(p)
-    mll = p.marginal_log_likelihood(D, constrainer, negative=True)
-    pre_mll_val = mll(params)
     optimiser = optax.adam(learning_rate=0.1)
-    optimised_params = optax_fit(mll, params, trainable_status, optimiser, n_iters=10)
+    optimised_params = fit(mll, params, trainable_status, optimiser, n_iters=10)
     optimised_params = transform(optimised_params, constrainer)
     assert isinstance(optimised_params, dict)
     assert mll(optimised_params) < pre_mll_val
@@ -60,7 +41,7 @@ def test_stop_grads(optim_pkg):
         )
     elif optim_pkg == "optax":
         optimiser = optax.adam(learning_rate=0.1)
-        learned_params = optax_fit(loss_fn, params, trainables, optimiser, n_iters=1)
+        learned_params = fit(loss_fn, params, trainables, optimiser, n_iters=1)
     assert learned_params["y"] == params["y"]
     assert learned_params["x"] != params["x"]
 
