fd33986b40e500e6029cd63cd0b13509900301a6
Thomas Pinder <pindert2@lancaster.ac.uk>
2022-07-11 13:27:47 +0000

    Bump Jax reqs

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ def parse_requirements_file(filename):
 
 
 REQUIRES = [
-    "jax==0.3.2",
-    "jaxlib==0.3.2",
+    "jax==0.3.4",
+    "jaxlib==0.3.4",
     "optax>=0.1.0",
     "chex==0.1.3",
     "distrax>=0.1.2",
