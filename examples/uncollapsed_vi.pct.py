0ec837c62c16d1829f15bd5a186e250abedac5e5
Thomas Pinder <tompinder@live.co.uk>
2023-04-10 22:11:29 +0100

    Fix missing jit import

--- examples/uncollapsed_vi.pct.py
+++ examples/uncollapsed_vi.pct.py
@@ -197,7 +197,7 @@ q = gpx.VariationalGaussian(posterior=p, inducing_inputs=z)
 # its negative.
 
 # %%
-negative_elbo = jax.jit(gpx.ELBO(negative=True))
+negative_elbo = jit(gpx.ELBO(negative=True))
 
 # %% [markdown]
 # ### Mini-batching
