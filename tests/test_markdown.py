e78861bbe7bcd9f52a9db7ba761b29c12cbc669b
Thomas Pinder <tompinder@live.co.uk>
2023-05-10 08:57:04 +0100

    Update markdown checks

--- tests/test_markdown.py
+++ tests/test_markdown.py
@@ -5,6 +5,16 @@ import pytest
 
 
 # Ensure that code chunks within any markdown files execute without error
-@pytest.mark.parametrize("fpath", pathlib.Path(".").glob("**/*.md"), ids=str)
-def test_files_good(fpath):
+@pytest.mark.parametrize("fpath", pathlib.Path("gpjax/").glob("**/*.md"), ids=str)
+def test_source_good(fpath):
+    check_md_file(fpath=fpath, memory=True)
+
+
+@pytest.mark.parametrize("fpath", pathlib.Path("docs").glob("**/*.md"), ids=str)
+def test_docs_good(fpath):
+    check_md_file(fpath=fpath, memory=True)
+
+
+@pytest.mark.parametrize("fpath", pathlib.Path(".").glob("*.md"), ids=str)
+def test_root_good(fpath):
     check_md_file(fpath=fpath, memory=True)
