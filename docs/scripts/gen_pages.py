c8a8df64e6361a9cbad41fa618b241ad832eb660
Thomas Pinder <tompinder@live.co.uk>
2023-05-10 22:27:28 +0100

    Make API TOC entries lead with a capital letter

--- docs/scripts/gen_pages.py
+++ docs/scripts/gen_pages.py
@@ -1,8 +1,36 @@
 from pathlib import Path
 
 import mkdocs_gen_files
+from mkdocs_gen_files import Nav
+from typing import Iterable, Mapping, Union
+import dataclasses
 
-nav = mkdocs_gen_files.Nav()
+
+class CustomNav(Nav):
+    def __init__(self):
+        super().__init__()
+
+    @dataclasses.dataclass
+    class Item:
+        level: int
+        title: str
+        filename: Union[str, None]
+
+    @classmethod
+    def _items(cls, data: Mapping, level: int) -> Iterable[Item]:
+        for key, value in data.items():
+            if key is not None:
+                if key == "gpjax":
+                    title = "GPJax"
+                elif key == "gps":
+                    title = "GPs"
+                else:
+                    title = key.title()
+                yield cls.Item(level=level, title=title, filename=value.get(None))
+                yield from cls._items(value, level + 1)
+
+
+nav = CustomNav()
 
 for path in sorted(Path("gpjax").rglob("*.py")):
     module_path = path.relative_to(".").with_suffix("")
@@ -19,10 +47,23 @@ for path in sorted(Path("gpjax").rglob("*.py")):
     elif parts[-1] == "__main__":
         continue
 
+    # final_part = parts[-1].title()
+    # parts = parts[:-1] + [final_part]
     nav[parts] = doc_path.as_posix()
-    # breakpoint()
+
+    # print(full_doc_path)
     with mkdocs_gen_files.open(full_doc_path, "w") as fd:
         identifier = ".".join(parts)
+        title = identifier.split(".")[-1].replace("_", " ").title()
+        if title == "Gps":
+            title = "GPs"
+        elif title == "Rbf":
+            title = "RBF"
+
+        if "Matern" in title:
+            title = title.replace("Matern", "Matérn")
+
+        print(f"# {title}\n", file=fd)
         print("::: " + identifier, file=fd)  #
 
     mkdocs_gen_files.set_edit_path(full_doc_path, path)
