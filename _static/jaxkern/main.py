18912d73ac7fe02ec927d338a82a23d2e9dd713a
thomaspinder <tompinder@live.co.uk>
2023-05-30 19:09:11 +0000

    Deploying to gh-pages from @ JaxGaussianProcesses/GPJax@134d80de7888f5192dd70b6c25eb87ce368a97b7 🚀

---
+++ _static/jaxkern/main.py
@@ -0,0 +1,59 @@
+# import jax.numpy as jnp
+# import jax.random as jr
+# import matplotlib as mpl
+# import matplotlib.patheffects as path_effects
+# import matplotlib.pyplot as plt
+
+# import gpjax.kernels as jk
+
+# key = jr.PRNGKey(123)
+
+
+# def set_font(font_path):
+#     font = mpl.font_manager.FontEntry(fname=font_path, name="my_font")
+#     mpl.font_manager.fontManager.ttflist.append(font)
+
+#     mpl.rcParams.update(
+#         {
+#             "font.family": font.name,
+#         }
+#     )
+
+
+# if __name__ == "__main__":
+#     set_font("lato.ttf")
+#     x1 = jnp.linspace(-3.0, 3.0, 500).reshape(-1, 1)
+
+#     kern = jk.Matern52()
+#     focal_points = [-2.5, 0.0, 2.5]
+#     cols = ["#5E97F6", "#30A89C", "#9C26B0"]
+
+#     fig, ax = plt.subplots(figsize=(6, 2.5), tight_layout=True)
+#     for c, f in zip(cols, focal_points):
+#         x2 = jnp.array([[0]])
+#         params = kern.init_params(key)
+#         Kxx = kern.cross_covariance(params, x1, x2)
+#         ax.plot(x1 + f, Kxx, color=c)
+#         ax.fill_between((x1 + f).squeeze(), Kxx.squeeze(), color=c, alpha=0.2)
+#     ax.axis("off")
+#     text = ax.text(
+#         x=0.0,
+#         y=0.25,
+#         s="JaxKern",
+#         fontsize=42,
+#         horizontalalignment="center",
+#         verticalalignment="center",
+#     )
+#     text.set_path_effects(
+#         [
+#             path_effects.Stroke(linewidth=3, foreground="white"),
+#             path_effects.Normal(),
+#         ]
+#     )
+#     plt.savefig(
+#         "logo.png",
+#         dpi=450,
+#         transparent=True,
+#         bbox_inches="tight",
+#         pad_inches=0.0,
+#     )
---
+++ examples/utils.py
@@ -0,0 +1,72 @@
+from matplotlib import transforms
+from matplotlib.patches import Ellipse
+import numpy as np
+
+
+def confidence_ellipse(x, y, ax, n_std=3.0, facecolor="none", **kwargs):
+    """
+    Create a plot of the covariance confidence ellipse of *x* and *y*.
+
+    Parameters
+    ----------
+    x, y : array-like, shape (n, )
+        Input data.
+
+    ax : matplotlib.axes.Axes
+        The axes object to draw the ellipse into.
+
+    n_std : float
+        The number of standard deviations to determine the ellipse's radiuses.
+
+    **kwargs
+        Forwarded to `~matplotlib.patches.Ellipse`
+
+    Returns
+    -------
+    matplotlib.patches.Ellipse
+    """
+    x = np.array(x)
+    y = np.array(y)
+    if x.size != y.size:
+        raise ValueError("x and y must be the same size")
+
+    cov = np.cov(x, y)
+    pearson = cov[0, 1] / np.sqrt(cov[0, 0] * cov[1, 1])
+    # Using a special case to obtain the eigenvalues of this
+    # two-dimensionl dataset.
+    ell_radius_x = np.sqrt(1 + pearson)
+    ell_radius_y = np.sqrt(1 - pearson)
+    ellipse = Ellipse(
+        (0, 0),
+        width=ell_radius_x * 2,
+        height=ell_radius_y * 2,
+        facecolor=facecolor,
+        **kwargs,
+    )
+
+    # Calculating the stdandard deviation of x from
+    # the squareroot of the variance and multiplying
+    # with the given number of standard deviations.
+    scale_x = np.sqrt(cov[0, 0]) * n_std
+    mean_x = np.mean(x)
+
+    # calculating the stdandard deviation of y ...
+    scale_y = np.sqrt(cov[1, 1]) * n_std
+    mean_y = np.mean(y)
+
+    transf = (
+        transforms.Affine2D()
+        .rotate_deg(45)
+        .scale(scale_x, scale_y)
+        .translate(mean_x, mean_y)
+    )
+
+    ellipse.set_transform(transf + ax.transData)
+    return ax.add_patch(ellipse)
+
+
+def clean_legend(ax):
+    handles, labels = ax.get_legend_handles_labels()
+    by_label = dict(zip(labels, handles))
+    ax.legend(by_label.values(), by_label.keys())
+    return ax
