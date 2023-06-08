7bfd88b32b746b0884b9e633b36f2ab053a8e190
Thomas Pinder <tompinder@live.co.uk>
2023-06-08 18:52:42 +0100

    Undo Yacht change

--- docs/examples/yacht.py
+++ docs/examples/yacht.py
@@ -1,19 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     cell_metadata_filter: -all
-#     custom_cell_magics: kql
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.11.2
-#   kernelspec:
-#     display_name: gpjax_beartype
-#     language: python
-#     name: python3
-# ---
-
 # %% [markdown]
 # # UCI Data Benchmarking
 #
