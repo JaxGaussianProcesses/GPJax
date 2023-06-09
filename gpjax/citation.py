f48368cbb6eca666a37c27a30bd7caf3784f2d91
Thomas Pinder <tompinder@live.co.uk>
2023-06-09 17:22:03 +0100

    Add citation structure

--- gpjax/citation.py
+++ gpjax/citation.py
@@ -1,7 +1,22 @@
-from beartype.typing import Union, Dict
+from dataclasses import (
+    dataclass,
+    fields,
+)
+
+from beartype.typing import (
+    Dict,
+    Union,
+)
 from plum import dispatch
 
-from gpjax.kernels import ArcCosine, GraphKernel, Matern12, Matern32, Matern52, RFF
+from gpjax.kernels import (
+    RFF,
+    ArcCosine,
+    GraphKernel,
+    Matern12,
+    Matern32,
+    Matern52,
+)
 from gpjax.objectives import (
     ELBO,
     CollapsedELBO,
@@ -15,123 +30,164 @@ MLLs = Union[ConjugateMLL, NonConjugateMLL, LogPosteriorDensity]
 CitationType = Union[str, Dict[str, str]]
 
 
-def _citation_as_str(citation: Dict[str, str]) -> str:
-    citation_str = f"@{citation['citation_type']}{{{citation['citation_key']},"
-    for k, v in citation.items():
-        if k not in ["citation_type", "citation_key"]:
-            citation_str += f"\n{k} = {{{v}}},"
-    return citation_str + "\n}"
+@dataclass(repr=False)
+class AbstractCitation:
+    citation_key: str = None
+    authors: str = None
+    title: str = None
+    year: str = None
+
+    def as_str(self) -> str:
+        citation_str = f"@{self.citation_type}{{{self.citation_key},"
+        for field in fields(self):
+            fn = field.name
+            if fn not in ["citation_type", "citation_key", "notes"]:
+                citation_str += f"\n{fn} = {{{getattr(self, fn)}}},"
+        return citation_str + "\n}"
+
+    def __repr__(self) -> str:
+        return repr(self.as_str())
+
+    def __str__(self) -> str:
+        return self.as_str()
 
 
+class NullCitation(AbstractCitation):
+    citation_key: str = None
+    authors: str = None
+    title: str = None
+    year: str = None
+
+    def __repr__(self) -> str:
+        return repr(
+            "No citation available. If you think this is an error, please open a pull"
+            " request."
+        )
+
+    def __str__(self) -> str:
+        return (
+            "No citation available. If you think this is an error, please open a pull"
+            " request."
+        )
+
+
+@dataclass
+class PhDThesisCitation(AbstractCitation):
+    school: str = None
+    institution: str = None
+    citation_type: str = "phdthesis"
+
+
+@dataclass
+class PaperCitation(AbstractCitation):
+    booktitle: str = None
+    citation_type: str = "inproceedings"
+
+
+@dataclass
+class BookCitation(AbstractCitation):
+    publisher: str = None
+    volume: str = None
+    citation_type: str = "book"
+
+
+####################
+# Default citation
+####################
 @dispatch
-def cite(tree) -> str:
-    return (
-        "No citation available. If you think this is an error, please open a pull"
-        " request."
-    )
+def cite(tree) -> NullCitation:
+    return NullCitation()
 
 
 ####################
 # Kernel citations
 ####################
-# @dispatch
-# def cite(tree: MaternKernels) -> str:
-#     return """@phdthesis{matern1960SpatialV,
-#     author      = {Bertil {M}atérn},
-#     school      = {Stockholm University},
-#     institution = {Stockholm University},
-#     title       = {Spatial variation : Stochastic models and their application to some problems in forest surveys and other sampling investigations},
-#     year        = {1960}
-#     }"""
-
-
 @dispatch
-def cite(tree: MaternKernels, as_str: bool = False) -> CitationType:
-    citation = {
-        "author": "Bertil Matern",
-        "school": "Stockholm University",
-        "institution": "Stockholm University",
-        "title": (
+def cite(tree: MaternKernels) -> PhDThesisCitation:
+    citation = PhDThesisCitation(
+        citation_key="matern1960SpatialV",
+        authors="Bertil Matérn",
+        title=(
             "Spatial variation : Stochastic models and their application to some"
             " problems in forest surveys and other sampling investigations"
         ),
-        "year": "1960",
-        "citation_type": "phdthesis",
-        "citation_key": "matern1960SpatialV",
-        "notes": ""
-    }
-    if as_str:
-        return _citation_as_str(citation)
-    else:
-        return citation
+        year="1960",
+        school="Stockholm University",
+        institution="Stockholm University",
+    )
+    return citation
 
 
 @dispatch
-def cite(tree: ArcCosine) -> str:
-    return """@inproceedings{cho2009kernel,
-    author = {Cho, Youngmin and Saul, Lawrence},
-    booktitle = {Advances in Neural Information Processing Systems},
-    title = {Kernel Methods for Deep Learning},
-    volume = {22},
-    year = {2009}
-    }"""
+def cite(tree: ArcCosine) -> PaperCitation:
+    return PaperCitation(
+        citation_key="cho2009kernel",
+        authors="Cho, Youngmin and Saul, Lawrence",
+        title="Kernel Methods for Deep Learning",
+        year="2009",
+        booktitle="Advances in Neural Information Processing Systems",
+    )
 
 
 @dispatch
-def cite(tree: GraphKernel) -> str:
-    return """@inproceedings{borovitskiy2021matern,
-    title     = {Matérn Gaussian processes on graphs},
-    author    = {Borovitskiy, Viacheslav and Azangulov, Iskander and Terenin, Alexander and Mostowsky, Peter and Deisenroth, Marc and Durrande, Nicolas},
-    booktitle = {International Conference on Artificial Intelligence and Statistics},
-    year      = {2021}
-    }"""
+def cite(tree: GraphKernel) -> PaperCitation:
+    return PaperCitation(
+        citation_key="borovitskiy2021matern",
+        title="Matérn Gaussian Processes on Graphs",
+        authors=(
+            "Borovitskiy, Viacheslav and Azangulov, Iskander and Terenin, Alexander and"
+            " Mostowsky, Peter and Deisenroth, Marc and Durrande, Nicolas"
+        ),
+        booktitle="International Conference on Artificial Intelligence and Statistics",
+        year="2021",
+    )
 
 
 @dispatch
-def cite(tree: RFF) -> str:
-    return """@article{rahimi2007random,
-    title={Random features for large-scale kernel machines},
-    author={Rahimi, Ali and Recht, Benjamin},
-    journal={Advances in neural information processing systems},
-    volume={20},
-    year={2007}
-    }
-    """
+def cite(tree: RFF) -> PaperCitation:
+    return PaperCitation(
+        citation_key="rahimi2007random",
+        authors="Rahimi, Ali and Recht, Benjamin",
+        title="Random features for large-scale kernel machines",
+        year="2007",
+        booktitle="Advances in neural information processing systems",
+        citation_type="article",
+    )
 
 
 ####################
 # Objective citations
 ####################
 @dispatch
-def cite(tree: MLLs) -> str:
-    return """@book{rasmussen2006gaussian,
-    title     = {Gaussian processes for machine learning},
-    author    = {Rasmussen, Carl Edward and Williams, Christopher K},
-    volume    = {2},
-    number    = {3},
-    year      = {2006},
-    publisher = {MIT press Cambridge, MA}
-    }"""
+def cite(tree: MLLs) -> BookCitation:
+    return BookCitation(
+        citation_key="rasmussen2006gaussian",
+        title="Gaussian Processes for Machine Learning",
+        authors="Rasmussen, Carl Edward and Williams, Christopher K",
+        year="2006",
+        publisher="MIT press Cambridge, MA",
+        volume="2",
+    )
 
 
 @dispatch
-def cite(tree: CollapsedELBO) -> str:
-    return """@inproceedings{titsias2009variational,
-    title        = {Variational learning of inducing variables in sparse Gaussian processes},
-    author       = {Titsias, Michalis},
-    booktitle    = {International Conference on Artificial Intelligence and Statistics},
-    pages        = {567--574},
-    year         = {2009},
-    organization = {PMLR}
-    }"""
+def cite(tree: CollapsedELBO) -> PaperCitation:
+    return PaperCitation(
+        citation_key="titsias2009variational",
+        title="Variational learning of inducing variables in sparse Gaussian processes",
+        authors="Titsias, Michalis",
+        year="2009",
+        booktitle="International Conference on Artificial Intelligence and Statistics",
+    )
 
 
 @dispatch
-def cite(tree: ELBO) -> str:
-    return """@article{hensman2013gaussian,
-    title   = {Gaussian processes for big data},
-    author  = {Hensman, James and Fusi, Nicolo and Lawrence, Neil D},
-    journal = {International Conference on Artificial Intelligence and Statistics},
-    year    = {2013}
-    }
-    """
+def cite(tree: ELBO) -> PaperCitation:
+    return PaperCitation(
+        citation_key="hensman2013gaussian",
+        title="Gaussian Processes for Big Data",
+        authors="Hensman, James and Fusi, Nicolo and Lawrence, Neil D",
+        year="2013",
+        booktitle="Uncertainty in Artificial Intelligence",
+        citation_type="article",
+    )
