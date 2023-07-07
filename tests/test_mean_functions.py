b3688b5c72ae965f48cd33963c3e6c76383c73b0
Thomas-Christie <thomashamish@hotmail.com>
2023-07-07 09:38:41 +0100

    Fix bug in zero mean function and add test

--- tests/test_mean_functions.py
+++ tests/test_mean_functions.py
@@ -1,13 +1,24 @@
+# Enable Float64 for more stable matrix inversions.
+from jax.config import config
+
+config.update("jax_enable_x64", True)
+
+
+from jax import jit
 import jax.numpy as jnp
+import jax.random as jr
 from jaxtyping import (
     Array,
     Float,
 )
+import optax as ox
 import pytest
 
+import gpjax as gpx
 from gpjax.mean_functions import (
     AbstractMeanFunction,
     Constant,
+    Zero,
 )
 
 
@@ -40,3 +51,37 @@ def test_constant(constant: Float[Array, " Q"]) -> None:
     assert (
         mf(jnp.array([[1.0, 2.0], [3.0, 4.0]])) == jnp.array([constant, constant])
     ).all()
+
+
+def test_zero_mean_remains_zero() -> None:
+    key = jr.PRNGKey(123)
+
+    x = jr.uniform(key=key, minval=0, maxval=1, shape=(20, 1))
+    y = jnp.full((20, 1), 50, dtype=jnp.float64)  # Dataset with non-zero mean
+    D = gpx.Dataset(X=x, y=y)
+
+    kernel = gpx.kernels.Constant(constant=jnp.array(0.0))
+    kernel = kernel.replace_trainable(
+        constant=False
+    )  # Prevent kernel from modelling non-zero mean
+    meanf = Zero()
+    prior = gpx.Prior(mean_function=meanf, kernel=kernel)
+    likelihood = gpx.Gaussian(num_datapoints=D.n, obs_noise=jnp.array(1e-6))
+    likelihood = likelihood.replace_trainable(obs_noise=False)
+    posterior = prior * likelihood
+
+    negative_mll = gpx.objectives.ConjugateMLL(negative=True)
+    negative_mll(posterior, train_data=D)
+    negative_mll = jit(negative_mll)
+
+    opt_posterior, history = gpx.fit(
+        model=posterior,
+        objective=negative_mll,
+        train_data=D,
+        optim=ox.adam(learning_rate=0.5),
+        num_iters=1000,
+        safe=True,
+        key=key,
+    )
+
+    assert opt_posterior.prior.mean_function.constant == 0.0
