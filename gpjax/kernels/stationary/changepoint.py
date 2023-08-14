25bc0b7162815412964d6580fdd04cdbe156f8e9
Jake Yeung <jake.yeung@ist.ac.at>
2023-08-14 16:21:22 +0200

    Add SwitchPoint kernel, where you can initialize with a list of two kernels. Update new imports, check they are sorted with isort

--- gpjax/kernels/stationary/changepoint.py
+++ gpjax/kernels/stationary/changepoint.py
@@ -16,18 +16,101 @@
 
 from dataclasses import dataclass
 
-from beartype.typing import Union
+from beartype.typing import (
+    Callable,
+    List,
+    Union,
+)
 import jax
 import jax.numpy as jnp
 from jaxtyping import Float
 import tensorflow_probability.substrates.jax.bijectors as tfb
 
 import gpjax as gpx
-from gpjax.base.param import param_field
+from gpjax.base import (
+    param_field,
+    static_field,
+)
+from gpjax.kernels.base import AbstractKernel
+
+# from gpjax.base.param import param_field
 from gpjax.typing import (
     Array,
     ScalarFloat,
 )
+
+
+@dataclass
+class SwitchKernel(AbstractKernel):
+    r"""A base class for products or sums of MeanFunctions."""
+
+    kernels: List[AbstractKernel] = None
+    operator: Callable = static_field(None)
+    tswitch: float = param_field(
+        jnp.float64(1.0), bijector=tfb.Identity(), trainable=False
+    )
+
+    def __post_init__(self):
+        # Add kernels to a list, flattening out instances of this class therein, as in GPFlow kernels.
+        kernels_list: List[AbstractKernel] = []
+
+        for kernel in self.kernels:
+            if not isinstance(kernel, AbstractKernel):
+                raise TypeError("can only combine Kernel instances")  # pragma: no cover
+
+            if isinstance(kernel, self.__class__):
+                kernels_list.extend(kernel.kernels)
+            else:
+                kernels_list.append(kernel)
+
+        self.kernels = kernels_list
+
+    def __call__(
+        self,
+        x: Float[Array, " D"],
+        y: Float[Array, " D"],
+    ) -> ScalarFloat:
+        r"""Evaluate the kernel on a pair of inputs.
+
+        Args:
+            x (Float[Array, " D"]): The left hand input of the kernel function.
+            y (Float[Array, " D"]): The right hand input of the kernel function.
+
+        Returns
+        -------
+            ScalarFloat: The evaluated kernel function at the supplied inputs.
+        """
+
+        def get_function_index(x, y, tswitch=1):
+            r"""
+            Specify four possible indices given x, y, and tswitch.
+
+            Args:
+                    x: Left hand argument of kernel function's call
+                    y: Right hand argument of kernel function's call
+                    tswitch: point at which to change to a different kernel
+            """
+            # Four possible indexes: 0, 1, 2, 3
+            indx = 3  # if indx doesn't get set to 0, 1, or 2, then default 3
+            # lessthan means that at tswitch, you are already switched
+            cond1 = jnp.less(x, tswitch)
+            cond2 = jnp.less(y, tswitch)
+            indx = jnp.where(jnp.logical_and(cond1, cond2), 0, indx)
+            indx = jnp.where(jnp.logical_and(jnp.invert(cond1), cond2), 1, indx)
+            indx = jnp.where(jnp.logical_and(cond1, jnp.invert(cond2)), 2, indx)
+            return indx.squeeze().astype("uint8")
+
+        def k_zero(x, y):
+            r"""Return 0 covariance"""
+            out = jnp.float64(0)
+            return out.squeeze()
+
+        indx = get_function_index(x, y, tswitch=self.tswitch)
+
+        flst = [self.kernels[0].__call__, k_zero, k_zero, self.kernels[1].__call__]
+        K = jax.lax.switch(indx, flst, x, y)
+
+        return K.squeeze()
 
 
 @dataclass
