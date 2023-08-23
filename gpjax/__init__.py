ab2b94c7644d7870b5de98e085224ce6756c22b5
Thomas Pinder <tompinder@live.co.uk>
2023-08-23 08:31:07 +0200

    Drop dynamic versioning

--- gpjax/__init__.py
+++ gpjax/__init__.py
@@ -75,7 +75,7 @@ __license__ = "MIT"
 __description__ = "Didactic Gaussian processes in JAX"
 __url__ = "https://github.com/JaxGaussianProcesses/GPJax"
 __contributors__ = "https://github.com/JaxGaussianProcesses/GPJax/graphs/contributors"
-__version__ = "0.0.0"
+__version__ = "0.6.8"
 
 __all__ = [
     "Module",
