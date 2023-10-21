83571b4fc4e205aa7bf5bd702eb5ee19ffc6e544
hmoss <32096840+henrymoss@users.noreply.github.com>
2023-10-21 09:19:49 +0100

    wip

--- gpjax/fit.py
+++ gpjax/fit.py
@@ -12,10 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-from dataclasses import (
-    asdict,
-    dataclass,
-)
+# from dataclasses import (
+#     asdict,
+#     dataclass,
+# )
 
 from beartype.typing import (
     Any,
