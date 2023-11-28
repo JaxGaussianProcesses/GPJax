175d56bf54eaf5e0b52d0aada637a966150ea0d1
Thomas Pinder <tompinder@live.co.uk>
2023-11-28 22:16:35 +0100

    Drop graph kernel jupytext

--- docs/examples/graph_kernels.py
+++ docs/examples/graph_kernels.py
@@ -1,20 +1,3 @@
-# -*- coding: utf-8 -*-
-# ---
-# jupyter:
-#   jupytext:
-#     cell_metadata_filter: -all
-#     custom_cell_magics: kql
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.11.2
-#   kernelspec:
-#     display_name: gpjax
-#     language: python
-#     name: python3
-# ---
-
 # %% [markdown]
 # # Graph Kernels
 #
