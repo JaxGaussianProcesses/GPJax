0aa60e8275362890ebe434ae96da96537dcf5182
wejpurvis <williampurvis00@gmail.com>
2024-02-25 12:53:12 +0000

    Fixed mutable default value error in VelocityKernel dataclass
    Replaced direct assignments of RBF kernels in VelocityKernel dataclass with field(default_factory=...)
    oceanmodelling example now works with python 3.11

--- docs/examples/oceanmodelling.py
+++ docs/examples/oceanmodelling.py
@@ -195,10 +195,17 @@ dataset_ground_truth = dataset_3d(pos_test, vel_test)
 
 
 # %%
+from dataclasses import field
+
+
 @dataclass
 class VelocityKernel(gpx.kernels.AbstractKernel):
-    kernel0: gpx.kernels.AbstractKernel = gpx.kernels.RBF(active_dims=[0, 1])
-    kernel1: gpx.kernels.AbstractKernel = gpx.kernels.RBF(active_dims=[0, 1])
+    kernel0: gpx.kernels.AbstractKernel = field(
+        default_factory=lambda: gpx.kernels.RBF(active_dims=[0, 1])
+    )
+    kernel1: gpx.kernels.AbstractKernel = field(
+        default_factory=lambda: gpx.kernels.RBF(active_dims=[0, 1])
+    )
 
     def __call__(
         self, X: Float[Array, "1 D"], Xp: Float[Array, "1 D"]
@@ -429,8 +436,12 @@ plot_fields(dataset_ground_truth, dataset_train, dataset_latent_velocity)
 @dataclass
 class HelmholtzKernel(gpx.kernels.AbstractKernel):
     # initialise Phi and Psi kernels as any stationary kernel in gpJax
-    potential_kernel: gpx.kernels.AbstractKernel = gpx.kernels.RBF(active_dims=[0, 1])
-    stream_kernel: gpx.kernels.AbstractKernel = gpx.kernels.RBF(active_dims=[0, 1])
+    potential_kernel: gpx.kernels.AbstractKernel = field(
+        default_factory=lambda: gpx.kernels.RBF(active_dims=[0, 1])
+    )
+    stream_kernel: gpx.kernels.AbstractKernel = field(
+        default_factory=lambda: gpx.kernels.RBF(active_dims=[0, 1])
+    )
 
     def __call__(
         self, X: Float[Array, "1 D"], Xp: Float[Array, "1 D"]
