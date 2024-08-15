e38d5bb4c826f8629476434d850fb5b62cae31ee
Thomas Pinder <tompinder@live.co.uk>
2024-08-15 23:00:42 +0200

    Drop flax base

--- gpjax/flax_base/bijectors.py
+++
@@ -1,8 +0,0 @@
-import tensorflow_probability.substrates.jax.bijectors as tfb
-
-from gpjax.flax_base.types import BijectorLookupType
-
-Bijectors: BijectorLookupType = {
-    "real": tfb.Identity(),
-    "positive": tfb.Softplus(),
-}
