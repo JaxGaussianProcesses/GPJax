6eaab4aede338f7c6c98466aa3bb86b9c4defc33
theorashid <30835680+theorashid@users.noreply.github.com>
2024-09-18 20:18:36 +0100

    optional solver_algorithm parameter to sample_approx (#478)
    * - Added an optional solver algorithm parameter to the `ConjugatePosterior` class for solving the inverse of the covariance matrix.
    - Updated the `test_conjugate_posterior_sample_approx` test function to include the `solver_algorithm` parameter.
    
    * remove commented import line
    
    * bump the version

--- gpjax/__init__.py
+++ gpjax/__init__.py
@@ -40,7 +40,7 @@ __license__ = "MIT"
 __description__ = "Didactic Gaussian processes in JAX"
 __url__ = "https://github.com/JaxGaussianProcesses/GPJax"
 __contributors__ = "https://github.com/JaxGaussianProcesses/GPJax/graphs/contributors"
-__version__ = "0.9.1"
+__version__ = "0.9.2"
 
 __all__ = [
     "base",
