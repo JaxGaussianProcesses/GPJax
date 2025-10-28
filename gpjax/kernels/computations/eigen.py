587b1f7c672d9d36d420fc983836afb7e1eeca30
Syed Zayyan Masud <44922710+syedzayyan@users.noreply.github.com>
2025-10-28 05:47:48 +0000

    Graph kernel consistency with other kernels (#560)
    * toggle dark theme
    
    * graph variational families
    
    * variational family not working and example
    
    * graph svgp fix
    
    * more updates
    
    * dtype problem
    
    * kernel checks in variational family
    
    * removed graph gaussian
    
    * formatting issues
    
    * ran uv run poe format to fix
    
    * passing tests and formatting weirdness
    
    * subclassing and tests
    
    * segregated graph tests that still fail
    
    * graph tests, changes to parent variational class signatures and graph kernel to allow SVGP shapes and dtypes
    
    * docstring for new class
    
    * remove ensure_2d function
    
    * tolerant dimensions
    
    * graph kernel consistent with other kernels
    
    * meaningful name and circular import issue
    
    * version bump and typechecking magic

--- gpjax/kernels/computations/eigen.py
+++ gpjax/kernels/computations/eigen.py
@@ -15,7 +15,6 @@
 
 
 import beartype.typing as tp
-import jax.numpy as jnp
 from jaxtyping import (
     Float,
     Num,
@@ -39,17 +38,4 @@ class EigenKernelComputation(AbstractKernelComputation):
     def _cross_covariance(
         self, kernel: Kernel, x: Num[Array, "N D"], y: Num[Array, "M D"]
     ) -> Float[Array, "N M"]:
-        # Transform the eigenvalues of the graph Laplacian according to the
-        # RBF kernel's SPDE form.
-        S = jnp.power(
-            kernel.eigenvalues
-            + 2
-            * kernel.smoothness.value
-            / kernel.lengthscale.value
-            / kernel.lengthscale.value,
-            -kernel.smoothness.value,
-        )
-        S = jnp.multiply(S, kernel.num_vertex / jnp.sum(S))
-        # Scale the transform eigenvalues by the kernel variance
-        S = jnp.multiply(S, kernel.variance.value)
-        return kernel(x, y, S=S)
+        return kernel(x, y)
